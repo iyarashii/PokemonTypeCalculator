6741b190bfe6cbe384125dbc9155617c8299d86b
Iyarashii <strider1337v@gmail.com>
2022-05-21 02:31:41 +0200

    Fix header link to license

--- PokemonTypeCalc/MainActivity.cs
+++ PokemonTypeCalc/MainActivity.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0,
-// go to https://github.com/iyarashii/Gw2Sharp/blob/master/LICENSE for license details.
+// go to https://github.com/iyarashii/PokemonTypeCalculator/blob/master/LICENSE for license details.
 
 using Android.App;
 using Android.Widget;
